f0fb337fa3e8f2a07458825901e52d08820721d2
AyltonNazario <ayltonvieira@id.uff.br>
2022-01-24 16:38:53 -0300

    small carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+export interface CarouselItem {
+  description: string;
+  img: string;
+  buttonLink: string;
+  buttonText: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'umbracocms';
+
+  carouselItems: CarouselItem[] = [
+    { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut ante lorem.",
+      img: "assets/hi-tech-enterprise-smart-assistant.jpg",
+      buttonLink: "https://www.itau.com.br",
+      buttonText: "Saiba mais"
+    },
+    { description: "Aliquam ullamcorper laoreet malesuada. Aliquam metus nisi, posuere blandit dignissim sit amet, sodales non libero.",
+      img: "assets/lightbulb-illuminating-floor-background.jpg",
+      buttonLink: "https://www.itau.com.br",
+      buttonText: "Saiba mais"
+    }
+  ];
+  carouselImgNF: string = 'assets/images/carousel-img-notfound.png';
+  currentSlide: number = 0;
+
+  constructor(protected router: Router) {}
+
+  ngOnInit(): void {
+  }
+
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.carouselItems?.length - 1 : previous;
+  }
+
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.carouselItems?.length ? 0 : next;
+  }
+
+  setCurrent($event: any) {
+    this.currentSlide = +$event.target.id;
+  }
+
+  onLinkClick(link: string){
+    if (link) {
+      window.open(link, '_blank');
+    }
+  }
+
+  ngOnDestroy() {
+  }
 }
---
+++ src/app/core/services/content.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentService {
+
+  constructor(private httpClient: HttpClient,
+    private sanitizer: DomSanitizer) { }
+
+  private buildRequest(path: string, options?: any): any  {
+
+    return this.httpClient.get(`${environment.assetsURL}/${path}`, options);
+  }
+}
